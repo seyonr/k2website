761842af28bbcdfb24fce3db8c6348be696b1978
seyonr <seyonr@seyonrs-MacBook-Pro.local>
2024-04-23 20:16:26 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const serv_gd = document.getElementById("serv-gd");
+
+serv_gd.addEventListener("mouseover", function(event) {
+    serv_gd.style.backgroundColor = "#E6E6E6";
+    serv_gd.style.color = "black";
+    serv_gd.querySelector('ul').style.display = 'block';
+});
+
+serv_gd.addEventListener("mouseout", function(event) {
+    serv_gd.style.backgroundColor = "black";
+    serv_gd.style.color = "white";
+    serv_gd.querySelector('ul').style.display = 'none';
+});
+// ----------------------------------------------------
+// ----------------------------------------------------
+const serv_vp = document.getElementById("serv-vp");
+
+serv_vp.addEventListener("mouseover", function(event) {
+    serv_vp.style.backgroundColor = "#E6E6E6";
+    serv_vp.style.color = "black";
+    serv_vp.querySelector('ul').style.display = 'block';
+});
+
+serv_vp.addEventListener("mouseout", function(event) {
+    serv_vp.style.backgroundColor = "black";
+    serv_vp.style.color = "white";
+    serv_vp.querySelector('ul').style.display = 'none';
+});
+// ----------------------------------------------------
+// ----------------------------------------------------
+const serv_p = document.getElementById("serv-p");
+
+serv_p.addEventListener("mouseover", function(event) {
+    serv_p.style.backgroundColor = "#E6E6E6";
+    serv_p.style.color = "black";
+    serv_p.querySelector('ul').style.display = 'block';
+});
+
+serv_p.addEventListener("mouseout", function(event) {
+    serv_p.style.backgroundColor = "black";
+    serv_p.style.color = "white";
+    serv_p.querySelector('ul').style.display = 'none';
+});
+
+
+
+
+
+
+
+
+
+
